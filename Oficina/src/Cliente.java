de15b532765183951f1aa30f23c0ec3d3e47bf2c
juhmarinho <juhmarinho@juhmarinho-Inspiron-N4050>
2015-12-02 10:04:35 -0200

    Oficina cria Cliente, classe principal não funciona :)

--- Oficina/src/Cliente.java
+++ Oficina/src/Cliente.java
@@ -1,8 +1,59 @@
 
+
+
 public class Cliente {
-		private int cpf;
+	private String cpf;;
 		private String nome;
-		private String telefone;
 		private Endereco endereco;
+		private Contato contato;
 		
+		
+		public Cliente(String cpf,String nome, Contato contato, Endereco endereco){
+
+			this.cpf= cpf;
+			this.nome = nome;
+			this.contato = contato;
+			this.endereco= endereco;
+			
+		}
+
+		// ----------------- pegando informacoes -----------------
+		
+
+		public String solicitarNome(){
+
+			return nome;
+		}
+
+		public Contato solicitarContato(){
+
+			return contato;
+		}
+
+		public Endereco solicitarEndereco(){
+
+			return endereco;
+		}
+
+		public String solicitarCpf(){
+
+			return cpf;
+		}
+
+		// ----------------- alterando informacoes -----------------
+		public void alterarDados(String nome, Contato contato, Endereco endereco){
+
+			this.nome = nome;
+			this.contato = contato;
+			this.endereco= endereco;
+		}
+
+	
+		
+		// ----------------------------------
+		public String toString(){
+			
+			return "Nome: " + nome +"\nCpf: " + cpf + "\nContato: \n" + contato + "\nEndereco: \n" + endereco;
+		}
 }
+
